3ffe486ba97d3a49019cf048c84a7759a987b28b
schwarper <username@users.noreply.github.com>
2024-09-06 19:29:25 +0300

    Add tags api support

--- ChatFilter.cs
+++ ChatFilter.cs
@@ -1,57 +1,76 @@
 ﻿using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.UserMessages;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using TagsApi;
 
-namespace ChatFilter
+namespace ChatFilter;
+
+public class WordFilter : BasePluginConfig
 {
-    public class WordFilter : BasePluginConfig
+    [JsonPropertyName("filter_list")]
+    public List<string> FilterWordList { get; set; } = [];
+}
+
+public partial class ChatFilter : BasePlugin, IPluginConfig<WordFilter>
+{
+    public override string ModuleName => "Chat Filter";
+    public override string ModuleVersion => "1.0";
+    public override string ModuleAuthor => "Oylsister";
+
+    public WordFilter Config { get; set; } = new WordFilter();
+    [GeneratedRegex(@"\d+")] private static partial Regex NumberRegex();
+
+    public override void OnAllPluginsLoaded(bool hotReload)
     {
-        [JsonPropertyName("filter_list")]
-        public List<string> FilterWordList { get; set; } = [];
-    }
+        try
+        {
+            var api = ITagApi.Capability.Get();
 
-    public class ChatFilter : BasePlugin, IPluginConfig<WordFilter>
-    {
-        public override string ModuleName => "Chat Filter";
-        public override string ModuleVersion => "1.0";
-        public override string ModuleAuthor => "Oylsister";
+            if (api == null)
+            {
+                return;
+            }
 
-        public List<string> defaultFilter = ["fuck", "nigger", "niger"];
-        public WordFilter Config { get; set; } = new WordFilter();
-
-        public override void Load(bool hotReload)
+            api.OnPlayerChatPre += um =>
+            {
+                OnMessage(um);
+            };
+        }
+        catch (Exception)
         {
             HookUserMessage(118, OnMessage, HookMode.Pre);
         }
+    }
 
-        public void OnConfigParsed(WordFilter config)
+    public void OnConfigParsed(WordFilter config)
+    {
+        Config = config;
+    }
+
+    private HookResult OnMessage(UserMessage um)
+    {
+        var message = um.ReadString("param2");
+        var originalMessage = message;
+
+        foreach (string word in Config.FilterWordList)
         {
-            if (config.FilterWordList.Count <= 0)
-            {
-                config.FilterWordList = defaultFilter;
-            }
-
-            Config = config;
+            string replacement = new('*', word.Length);
+            message = message.Replace(word, replacement, StringComparison.OrdinalIgnoreCase);
         }
 
-        public HookResult OnMessage(UserMessage um)
+        string ipOrNumberPattern = @"\b(?:\d{1,3}[.\s/]){3}\d{1,3}\b";
+        message = Regex.Replace(message, ipOrNumberPattern, match =>
         {
-            string message = um.ReadString("param2");
-            string originalMessage = message;
+            return NumberRegex().Replace(match.Value, m => new string('*', m.Value.Length));
+        }, RegexOptions.IgnoreCase);
 
-            foreach (string word in Config.FilterWordList)
-            {
-                string replacement = new('*', word.Length);
-                message = message.Replace(word, replacement, StringComparison.OrdinalIgnoreCase);
-            }
-
-            if (message != originalMessage)
-            {
-                um.SetString("param2", message);
-                return HookResult.Changed;
-            }
-
-            return HookResult.Continue;
+        if (message != originalMessage)
+        {
+            um.SetString("param2", message);
+            return HookResult.Changed;
         }
+
+        return HookResult.Continue;
     }
 }
